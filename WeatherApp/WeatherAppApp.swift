c2f0d7308c533409131e5d531f9b564547613115
nasim-ahmed <nasim.ahmed54@gmail.com>
2021-11-13 10:28:56 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nasim Ahmed on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
